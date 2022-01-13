987e09ace807ca72ba0150048b724061fb1aa018
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2022-01-13 16:20:40 +0530

    Test Page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LandingPage from "./pages/LandingPage";
 import About from "./pages/About";
 import Mentors from "./pages/Mentors";
 import FaqPage from "./pages/FaqPage";
+import Tests from "./pages/Tests";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -16,6 +17,7 @@ const App = () => {
           <Route exact path="/about" element={<About />} />
           <Route exact path="/mentors" element={<Mentors />} />
           <Route exact path="/faq" element={<FaqPage />} />
+          <Route exact path="/tests" element={<Tests />} />
         </Routes>
       </Router>
     </div>
