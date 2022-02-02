af7fb1104d6927831cce837655629c5599293b0d
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2022-02-02 23:26:31 +0530

    Correct (#20)
    * responsive landing page
    
    * Full responsive
    
    * Tablet responsive
    
    * improvement
    
    * improve
    
    * improvement

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -50,7 +50,6 @@ const Courses = () => {
               <Link
                 to="/mas101"
                 className="text-sm lg:hidden md:left-96 md:bottom-1  font-medium px-4 py-3 relative left-40 bottom-14 rounded-xl text-white bg-blue-600"
-
               >
                 Explore Now
               </Link>
@@ -62,15 +61,12 @@ const Courses = () => {
                 </p>
 
                 <p className="lg:text-[22px] md:left-72 text-lg font-semibold relative lg:left-48 left-52 text-white lg:bottom-[8px] bottom-4 z-10">
-
                   Exclusive
                 </p>
                 <img
                   src={exclusive}
                   alt="exclusive"
-
                   className="relative left-[142px] md:left-[216px] lg:left-[142px] lg:bottom-10 bottom-12"
-
                 />
               </div>
               <img
@@ -104,10 +100,14 @@ const Courses = () => {
               <p className="text-[15px] font-semibold text-blue-600 mb-5">
                 ₹ 2500 + ISA
               </p>
-              <Link>
-                to="/mas102" className="text-sm lg:hidden md:left-96 md:bottom-1
+              <Link
+                to="/mas102"
+                className="text-sm lg:hidden md:left-96 md:bottom-1
                 font-medium px-4 py-3 relative left-40 bottom-14 rounded-xl
-                text-white bg-blue-600" Explore Now
+                text-white bg-blue-600"
+              >
+                {" "}
+                Explore Now
 
               </Link>
             </div>
