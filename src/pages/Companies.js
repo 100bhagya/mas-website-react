85162e048c414e3f6a42a21bfa75d7ebba9b0539
Harry <harikeshkrsingh20@gmail.com>
2023-01-29 19:35:51 +0530

    modified hero section

--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -28,6 +28,8 @@ import TestimonialNew from "../components/TestimonialNew";
 import CollegeData from "../data/College";
 import CFQData from "../data/CompaniesFaqData";
 import CompanyData from "../data/Company";
+import design2 from "../images/Vector 3.png";
+
 
 function FAQ({ faq, index, toggleFAQ, tag }) {
   return (
@@ -130,223 +132,233 @@ const Companies = () => {
     <Fragment>
       <Navbar />
 
-     
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          slidesPerView={1}
-          navigation
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-          pagination={{ clickable: true }}
-          // className="absolute bottom-40 md:static md:bottom-0"
-        >
-          <SwiperSlide className="cursor-pointer">
-            <div className="w-full h-full bg-gradient-to-b from-blue-300 to-white"></div>
-            <div>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        navigation
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        pagination={{ clickable: true }}
+        // className="absolute bottom-40 md:static md:bottom-0"
+      >
+        <SwiperSlide className="cursor-pointer">
+          <div className="flex flex-col-reverse w-full h-full pt-16 lg:flex-row bg-gradient-to-b from-blue-200 via-blue-100 to-blue-50">
+            <div className="max-w-lg relative bottom-[12vh] md:static px-4 mx-auto lg:flex lg:items-center pt-9 md:max-w-xl lg:max-w-4xl">
+              <div className="lg:pt-16 bottom-3 md:top-8 lg:pb-32 pb-10 relative lg:left-[7%] lg:top-3 text-center lg:text-left">
+                <h1 className="relative text-2xl  leading-relaxed text-black md:bottom-[13vh] lg:font-normal font-black md:text-3xl lg:text-9xl font-primary">
+                  MISSION
+                </h1>
+                <p className="lg:pt-6 md:font-medium text-sm  text-[#787B7D]  lg:text-lg relative md:bottom-[10vh]  leading-loose lg:w-[68%]  ">
+                  MAS aims to turn potential into success for students seeking
+                  to become world-class professionals. Our program is designed
+                  to provide efficient, affordable and industry-centric guidance
+                  to bridge the gap between student potential and professional
+                  acclaim.
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center">
               <img
-                className="absolute top-0  md:top-[7vh] md:left-[22vw] invisible md:visible lg:visible lg:w-[80vw]  lg:left-[35vw]"
                 src={Mission}
-                alt="mission"
+                alt=""
+                className="lg:absolute absolute md:top-[3vh] bottom-[5vh] design1  z-10 lg:left-[50vw] lg:top-[5vw] lg:h-[80%] h-[250px] top-7"
               />
-              <p className="absolute text-6xl font-black text-black left-5 md:text-9xl top-16 md:left-10 md:top-28">
-                MISSION
-              </p>
-              <p className="absolute w-2/3 text-black left-5 md:w-1/3 top-48 md:left-10 md:top-1/2 ">
-                MAS aims to turn potential into success for students seeking to
-                become world-class professionals. Our program is designed to
-                provide efficient, affordable and industry-centric guidance to
-                bridge the gap between student potential and professional
-                acclaim.
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="cursor-pointer">
-            <div className="w-full h-full bg-gradient-to-b from-blue-300 to-white"></div>
-            <img
-              className="absolute top-0 md:left-[38vw] md:w-7/12  lg:w-auto  md:top-[5vh] invisible md:visible lg:visible  lg:top-20 lg:left-[60vw]"
-              src={Vision}
-              alt="mission"
-            />
-            <p className="absolute text-6xl font-black text-black left-5 md:text-9xl top-16 md:left-10 md:top-28">
-              VISION
-            </p>
-            <p className="absolute w-2/3 text-black left-5 md:w-1/3 top-48 md:left-10 md:top-1/2 ">
-              We are optimistic about the future of analytics in India and are
-              working to develop a next-generation workforce that meets the
-              needs of the industry, both within India and globally.
-            </p>
-          </SwiperSlide>
-        </Swiper>
-        <a
-          href=""
-          target=""
-          rel="noopener noreferrer"
-          className="relative z-10 p-2 text-white bg-blue-600 border border-transparent rounded-lg cursor-pointer md:bottom-12 lg:bottom-20 md:p-3 bottom-20 left-5 md:left-10"
-        >
-          Hire Now
-        </a>
-        <Organization
-          props={"Trusted by leading Organizations"}
-          data={CompanyData}
-        />
-        <Organization
-          props={"Partnered with prestigious colleges"}
-          data={CollegeData}
-        />
-        <section className="p-12 bg-white">
-          <h1 className="py-12 text-xl text-center text-gray-900 lg:text-3xl">
-            Why Hire Us?
-          </h1>
-          <div className="flex flex-col space-y-8">
-            {HRData.map((data, i) => {
-              return (
-                <Card1
-                  img={data.image}
-                  title={data.heading}
-                  content={data.text}
-                  key={i}
-                />
-              );
-            })}
-          </div>
-        </section>
-        <MentorSection />
-        <section>
-          <div className="mt-16 text-3xl text-center ">Track Record</div>
-          <div className="flex flex-wrap mt-16 ml-7 md:ml-20 track-record">
-            {Data.map((data, i) => {
-              return <Card key={i} data={data} />;
-            })}
-          </div>
-        </section>
-        <section>
-          <div className="mt-16 text-3xl text-center ">Our Journey</div>
-          <div className="mt-16 track-record">
-            <div className="flex flex-wrap py-12 space-y-24 sm:space-y-0 place-content-around container-1 px-7">
-              <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
-                <div className="">
-                  <FaUserGraduate className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>
-                  10000+ Students Upskilled in Analytics from IIT’s, NIT’s and
-                  Tier 1 institutes in past 20 Months
-                </h1>
-              </div>
-              <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
-                <div className="">
-                  <ImUserPlus className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>
-                  Students spend 15 Lakhs minutes on Testing themselves on Test
-                  Platform
-                </h1>
-              </div>
-              <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
-                <div className="">
-                  <MdPlayLesson className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>
-                  Students consumed 9300 Hours of Content on Learn Platform
-                </h1>
-              </div>
-              <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
-                <div className="">
-                  <BsBank2 className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>
-                  Collaborated with top 100 colleges of India NIRF Rank (
-                  {string})
-                </h1>
-              </div>
-            </div>
-            <div className="flex flex-wrap py-12 space-y-24 md:space-y-0 place-content-around container-2 ">
-              <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
-                <div className="">
-                  <BsBagCheckFill className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>26 Crore worth Placement Done</h1>
-              </div>
-              <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
-                <div className="">
-                  <FaChalkboardTeacher className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>Strong force of 300+ Expert Mentors from IIT’s</h1>
-              </div>
-              <div className="flex flex-col w-64 space-y-3.5 cursor-pointer ">
-                <div className="">
-                  <ImOffice className="w-8 h-8 text-blue-900" />
-                </div>
-                <h1>MAS alums working in 250+ Companies</h1>
-              </div>
+              <img
+                src={design2}
+                alt=""
+                className="relative hidden design2 lg:bottom-16 lg:block"
+              />
             </div>
           </div>
-        </section>
-        <TestimonialNew />
-        <section className="py-16 bg-white">
-          <div className="flex flex-col items-center justify-center p-12 mx-auto space-y-6 shadow-2xl md:flex-row md:space-y-0 md:space-x-8 bg-works rounded-xl lg:w-4/5">
-            <div className="flex flex-col space-y-4 md:w-1/2">
-              <h2 className="pb-4 font-serif text-3xl font-bold text-center text-black md:text-left">
-                From The Founder’s Desk
-              </h2>
-              <p className="text-sm md:text-base">
-                My Analytics School is an Ed-Tech start-up founded by IIT
-                alumni, providing an end-to-end solution for Analytics, DS
-                Placements and related job preparation. "MAS screens top college
-                candidates and upskills them with industry experts to provide
-                recruiters with a pool of highly skilled candidates, saving time
-                on the hiring process and making the freshers hiring process
-                efficient"
-              </p>
+        </SwiperSlide>
+        <SwiperSlide className="cursor-pointer">
+          <div className="flex flex-col-reverse w-full h-full pt-16 lg:flex-row bg-gradient-to-b from-blue-200 via-blue-100 to-blue-50">
+            <div className="max-w-lg px-4 mx-auto relative bottom-[15vh]   lg:flex lg:items-center pt-9 md:max-w-xl lg:max-w-4xl">
+              <div className="lg:pt-16 bottom-3 md:top-[17vh]  lg:pb-32 pb-10 relative lg:left-[7%] lg:top-[15vh] text-center lg:text-left">
+                <h1 className="relative text-3xl  leading-relaxed text-black md:bottom-[15vh] lg:font-normal font-black md:text-3xl lg:text-9xl font-primary">
+                  VISION
+                </h1>
+                <p className="lg:pt-6 md:font-medium text-sm  text-[#787B7D]  lg:text-lg relative md:bottom-[10vh]  leading-loose lg:w-[68%]">
+                  We are optimistic about the future of analytics in India and
+                  are working to develop a next-generation workforce that meets
+                  the needs of the industry, both within India and globally.
+                </p>
+              </div>
             </div>
-            <iframe
-              width="590"
-              height="340"
-              src="https://www.youtube.com/embed/pKyiLrMLQ80"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-              className=" rounded-2xl"
-            ></iframe>
+            <div className="flex justify-center">
+              <img
+                src={Vision}
+                alt=""
+                className="lg:absolute  absolute design1  z-10 md:top-[4vh] lg:right-16 lg:top-[100px] lg:h-[70%] h-[250px] top-10"
+              />
+              <img
+                src={design2}
+                alt=""
+                className="relative hidden design2 lg:bottom-16 lg:block"
+              />
+            </div>
           </div>
-        </section>
-        <section>
-          <div className="mt-16 text-3xl text-center ">FAQs</div>
-          {CFQData.map((post) => {
+        </SwiperSlide>
+      </Swiper>
+      <a
+        href=""
+        target=""
+        rel="noopener noreferrer"
+        className="relative z-10 p-2 text-white bg-blue-600 border border-transparent rounded-lg cursor-pointer       lg:bottom-40 md:p-3 md:bottom-[7vh] md:left-[30vh] bottom-[15vh] left-[37vw] lg:left-20"
+      >
+        Hire Now
+      </a>
+      <Organization
+        props={"Trusted by leading Organizations"}
+        data={CompanyData}
+      />
+      <Organization
+        props={"Partnered with prestigious colleges"}
+        data={CollegeData}
+      />
+      <section className="p-12 bg-white">
+        <h1 className="py-12 text-xl text-center text-gray-900 lg:text-3xl">
+          Why Hire Us?
+        </h1>
+        <div className="flex flex-col space-y-8">
+          {HRData.map((data, i) => {
             return (
-              <div
-                key={post.id}
-                className="flex flex-wrap pb-12 mx-14 place-content-around faqs "
-              >
-                {post.questions.map((faq, i) => (
-                  <FAQ
-                    faq={faq}
-                    tag={post.tag}
-                    index={i}
-                    toggleFAQ={toggleFAQ}
-                  />
-                ))}
-              </div>
+              <Card1
+                img={data.image}
+                title={data.heading}
+                content={data.text}
+                key={i}
+              />
             );
           })}
-          <a
-            href="/mentors"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="relative mb-20 text-blue-800 border-b-2 border-transparent left-40 bottom-10 md:left-3/4 hover:border-blue-800"
-          >
-            Get More Questions?
-          </a>
-        </section>
+        </div>
+      </section>
+      <MentorSection />
+      <section>
+        <div className="mt-16 text-3xl text-center ">Track Record</div>
+        <div className="flex flex-wrap mt-16 ml-7 md:ml-20 track-record">
+          {Data.map((data, i) => {
+            return <Card key={i} data={data} />;
+          })}
+        </div>
+      </section>
+      <section>
+        <div className="mt-16 text-3xl text-center ">Our Journey</div>
+        <div className="mt-16 track-record">
+          <div className="flex flex-wrap py-12 space-y-24 sm:space-y-0 place-content-around container-1 px-7">
+            <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
+              <div className="">
+                <FaUserGraduate className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>
+                10000+ Students Upskilled in Analytics from IIT’s, NIT’s and
+                Tier 1 institutes in past 20 Months
+              </h1>
+            </div>
+            <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
+              <div className="">
+                <ImUserPlus className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>
+                Students spend 15 Lakhs minutes on Testing themselves on Test
+                Platform
+              </h1>
+            </div>
+            <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
+              <div className="">
+                <MdPlayLesson className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>Students consumed 9300 Hours of Content on Learn Platform</h1>
+            </div>
+            <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
+              <div className="">
+                <BsBank2 className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>
+                Collaborated with top 100 colleges of India NIRF Rank ({string})
+              </h1>
+            </div>
+          </div>
+          <div className="flex flex-wrap py-12 space-y-24 md:space-y-0 place-content-around container-2 ">
+            <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
+              <div className="">
+                <BsBagCheckFill className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>26 Crore worth Placement Done</h1>
+            </div>
+            <div className=" flex flex-col w-64 space-y-3.5 cursor-pointer">
+              <div className="">
+                <FaChalkboardTeacher className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>Strong force of 300+ Expert Mentors from IIT’s</h1>
+            </div>
+            <div className="flex flex-col w-64 space-y-3.5 cursor-pointer ">
+              <div className="">
+                <ImOffice className="w-8 h-8 text-blue-900" />
+              </div>
+              <h1>MAS alums working in 250+ Companies</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <TestimonialNew />
+      <section className="py-16 bg-white">
+        <div className="flex flex-col items-center justify-center p-12 mx-auto space-y-6 shadow-2xl md:flex-row md:space-y-0 md:space-x-8 bg-works rounded-xl lg:w-4/5">
+          <div className="flex flex-col space-y-4 md:w-1/2">
+            <h2 className="pb-4 font-serif text-3xl font-bold text-center text-black md:text-left">
+              From The Founder’s Desk
+            </h2>
+            <p className="text-sm md:text-base">
+              My Analytics School is an Ed-Tech start-up founded by IIT alumni,
+              providing an end-to-end solution for Analytics, DS Placements and
+              related job preparation. "MAS screens top college candidates and
+              upskills them with industry experts to provide recruiters with a
+              pool of highly skilled candidates, saving time on the hiring
+              process and making the freshers hiring process efficient"
+            </p>
+          </div>
+          <iframe
+            width="590"
+            height="340"
+            src="https://www.youtube.com/embed/pKyiLrMLQ80"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            className=" rounded-2xl"
+          ></iframe>
+        </div>
+      </section>
+      <section>
+        <div className="mt-16 text-3xl text-center ">FAQs</div>
+        {CFQData.map((post) => {
+          return (
+            <div
+              key={post.id}
+              className="flex flex-wrap pb-12 mx-14 place-content-around faqs "
+            >
+              {post.questions.map((faq, i) => (
+                <FAQ faq={faq} tag={post.tag} index={i} toggleFAQ={toggleFAQ} />
+              ))}
+            </div>
+          );
+        })}
+        <a
+          href="/mentors"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative mb-20 text-blue-800 border-b-2 border-transparent left-40 bottom-10 md:left-3/4 hover:border-blue-800"
+        >
+          Get More Questions?
+        </a>
+      </section>
 
-        <Footer />
-      
+      <Footer />
     </Fragment>
   );
 };
 
 export default Companies;
-
