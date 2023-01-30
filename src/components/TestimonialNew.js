f80e11265060ec0968838fa9a49113180ef0b1f5
Harry <harikeshkrsingh20@gmail.com>
2023-01-30 15:26:25 +0530

    modified testimonial speed

--- src/components/TestimonialNew.js
+++ src/components/TestimonialNew.js
@@ -5,7 +5,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Data from "../data/Data";
-const Mentor = () => {
+const Testimonials = () => {
   const settings = {
     dots: true,
     infinite: true,
@@ -13,20 +13,16 @@ const Mentor = () => {
     slidesToScroll: 1,
     autoplay: true,
     speed: 2000,
-    autoplaySpeed: 5000,
-    pauseOnHover: true,
-    pauseOnMouseEnter: true,
     cssEase: "linear",
 
     responsive: [
-      
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          autoplaySpeed: 7000,
+
           speed: 2000,
           dots: true,
         },
@@ -37,7 +33,7 @@ const Mentor = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           speed: 2000,
-          autoplaySpeed: 6000,
+
           dots: false,
         },
       },
@@ -47,7 +43,7 @@ const Mentor = () => {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          autoplaySpeed: 6000,
+
           speed: 2000,
           dots: false,
         },
@@ -58,8 +54,8 @@ const Mentor = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          autoplaySpeed: 6000,
-          speed:1000,
+
+          speed: 2000,
           dots: false,
         },
       },
@@ -70,37 +66,39 @@ const Mentor = () => {
       <div className="mt-16 text-3xl text-center">Testimonials</div>
 
       <div className="w-full py-8 my-12 space-y-2 text-center">
-        <Slider
-          {...settings}
-          className="absolute left-3 md:static md:left-0"
-        >
+        <Slider {...settings} className="absolute left-3 md:static md:left-0">
           {Data.map((data) => (
-             <div key={data.id}>
-            <div className="mb-32 mt-28">
-              <div className="box-border flex flex-col px-8 py-4 mt-5 space-x-12 border-2 shadow-xl cursor-pointer lg:transition lg:duration-500 lg:transform bg-blue-50 place-content-around md:w-auto lg:hover:shadow-slate-500 lg:hover:-translate-y-3 rounded-2xl h-42 mx-7 border-slate-300 card">
-                <div className="relative items-center w-full mt-10 bottom-10">
-                  <div className="relative  flex flex-col bottom-16 lg:bottom-20 mt-5w-full h-[11vh]">
-                    <img className="relative w-4/12 left-16 md:left-20 lg:left-28 " src={data.image} alt="img" />
-                    <div>
-                      <h1 className="text-xl lg:text-xl md:text-2xl lg:mt-0 md:mt-5">{data.name}</h1>
-                      <h3 className="text-base lg:text-base md:text-lg text-slate-500">{data.position}</h3>
+            <div key={data.id}>
+              <div className="mb-32 mt-28">
+                <div className="box-border flex flex-col px-8 py-4 mt-5 space-x-12 border-2 shadow-xl cursor-pointer lg:transition lg:duration-500 lg:transform bg-blue-50 place-content-around md:w-auto lg:hover:shadow-slate-500 lg:hover:-translate-y-3 rounded-2xl h-42 mx-7 border-slate-300 card">
+                  <div className="relative items-center w-full mt-10 bottom-10">
+                    <div className="relative  flex flex-col bottom-16 lg:bottom-20 mt-5w-full h-[11vh]">
+                      <img
+                        className="relative w-4/12 left-16 md:left-20 lg:left-28 "
+                        src={data.image}
+                        alt="img"
+                      />
+                      <div>
+                        <h1 className="text-xl lg:text-xl md:text-2xl lg:mt-0 md:mt-5">
+                          {data.name}
+                        </h1>
+                        <h3 className="text-base lg:text-base md:text-lg text-slate-500">
+                          {data.position}
+                        </h3>
+                      </div>
                     </div>
-                  </div>
-                  <div className="overflow-y-auto  md:text-lg lg:text-sm  rounded-2xl bg-gradient-to-b from-blue-50 to-blue-100 p-6  mt-5 text-sm  w-full  h-[40vh]">
-                    {data.about}
+                    <div className="overflow-y-auto  md:text-lg lg:text-sm  rounded-2xl bg-gradient-to-b from-blue-50 to-blue-100 p-6  mt-5 text-sm  w-full  h-[40vh]">
+                      {data.about}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            </div>
           ))}
         </Slider>
       </div>
-      
-     
     </>
   );
 };
 
-export default Mentor;
-
+export default Testimonials;
