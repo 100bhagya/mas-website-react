5889811dd0a7a589af475151d63e54b4c03dffe0
Harikesh Singh <91447902+Harikesh04@users.noreply.github.com>
2023-04-04 22:46:00 +0530

    added 10s timer to render the AI Bot (#63)

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -1,16 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 function Bot() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://www.kenyt.ai/botapp/ChatbotUI/dist/js/bot-loader.js';
-    script.type = 'text/javascript';
-    script.dataset.bot = '283540085';
+  const script = document.createElement("script");
+  script.src = "https://www.kenyt.ai/botapp/ChatbotUI/dist/js/bot-loader.js";
+  script.type = "text/javascript";
+  script.dataset.bot = "283540085";
+
+  function AddBot() {
     document.body.appendChild(script);
+  }
+
+  function RemoveBot() {
+    document.body.removeChild(script);
+  }
+
+  useEffect(() => {
+    setTimeout(AddBot, 10000);
 
     return () => {
-      document.body.removeChild(script);
-    }
+      RemoveBot();
+    };
   }, []);
 
   return null;
